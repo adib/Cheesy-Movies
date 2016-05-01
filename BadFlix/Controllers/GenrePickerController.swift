c14bf2e612ca6d814a659bb396840aae55171ddf
Sasmito Adibowo <adib@basil-salad.com>
2016-05-01 10:12:49 +0800

    Genre list populated

--- BadFlix/Controllers/GenrePickerController.swift
+++ BadFlix/Controllers/GenrePickerController.swift
@@ -10,19 +10,61 @@ import UIKit
 
 class GenrePickerController: NSObject,UIPickerViewDataSource,UIPickerViewDelegate {
 
+    let genreList = GenreList.defaultInstance
+    
+    var selectionList : [(String,GenreEntity?)]?
+    
     @IBOutlet weak var pickerView: UIPickerView?
+    
+    func reloadData() {
+        guard let genreMappings = genreList.currentMapping else {
+            genreList.refresh({
+                [weak self] (error) in
+                guard error == nil else {
+                    return
+                }
+                self?.reloadData()
+            })
+            return
+        }
+        
+        var updatingSelectionList = Array<(String,GenreEntity?)>()
+        updatingSelectionList.reserveCapacity(genreMappings.count + 1)
+        
+        for (_,genre) in genreMappings {
+            if let title = genre.title {
+                updatingSelectionList.append((title,genre))
+            }
+        }
+        updatingSelectionList.sortInPlace( {
+            $0.0 > $1.0
+        })
+        updatingSelectionList.insert((NSLocalizedString("All Genres", comment: "Genre Entry"),nil), atIndex: 0)
+        self.selectionList = updatingSelectionList
+        
+        pickerView?.reloadAllComponents()
+    }
     
     // MARK: - Picker View
     
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        self.pickerView = pickerView
         return 1
     }
     
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 10
+        guard let items = selectionList else {
+            self.reloadData()
+            return 0
+        }
+        return items.count
     }
     
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return "Item: \(row)"
+        guard let items = selectionList else {
+            return ""
+        }
+
+        return items[row].0
     }
 }
